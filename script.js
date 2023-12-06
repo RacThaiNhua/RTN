6fdb2299581fb6569e4fba71f15dbc931bd198a6
BaoNguyenFUNiX <baonpnfx28540@funix.edu.vn>
2023-12-06 21:36:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// const links = document.querySelectorAll('.nav-btn');
+    
+// if (links.length) {
+//   links.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//       links.forEach((link) => {
+//           link.classList.remove('active');
+//       });
+//       e.preventDefault();
+//       link.classList.add('active');
+//     });
+//   });
+// }
+
+
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
